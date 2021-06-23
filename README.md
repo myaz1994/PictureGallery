4a0b0fc77fc11a30cbb4124816249b3041183ac4
myazelhoussaine <myazelhoussaine@gmail.com>
2021-06-24 00:18:49 +0100

    Added full Image view in Modal Bottom Sheet

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # Picture Gallery App
 
-A Picture Gallery App developed for recruitment purposes in Consider-IT
+A Picture Gallery App developed for Consider-IT recruiting purposes
 
 ## General Information
 
 The main features of this app are:
 - Build an image gallery that shows the pictures taken with the phone (Library used: https://pub.dev/packages/photo_gallery)
 - Show image thumbnails in a staggered grid view. (Library used: https://pub.dev/packages/flutter_staggered_grid_view)
-- When an image is clicked, the full image is presented in a detail view. (Not developped yet)
+- When an image is clicked, the full image is presented in a detail view using a Modal Bottom Sheet. (Library used: https://pub.dev/packages/photo_gallery)
 
-Developped with: Flutter / Dart
+Developed with: Flutter / Dart
 
-Developped by: El Houssaine Myaz
+Developed by: El Houssaine Myaz
 
 
 ## Change log
+
+> ## v0.0.3 (24/06/2021) 
+>
+> #### New Features:
+> 
+> - Added Image View to show the full image (Library used: https://pub.dev/packages/photo_gallery).
+> - The Image View is opened on the same page using a Modal Bottom Sheet with a Fade In Effect and a close button at the top.
+
 
 > ## v0.0.2 (23/06/2021)
 > 
@@ -22,7 +30,7 @@ Developped by: El Houssaine Myaz
 > 
 > - Changed AppBar to a container box with a centered page title and circular borders and grandient blue colors.
 > - Changed the background of the App to light blue.
-> - Pictures are shown in a Stagerred Grid View with circular borders after fading effects (Library used: https://pub.dev/packages/flutter_staggered_grid_view)
+> - Pictures are shown in a Staggered Grid View with circular borders after fading effects (Library used: https://pub.dev/packages/flutter_staggered_grid_view).
 > - While loading pictures, empty boxes are shown with circular borders and blue gradient colors.
 
 
@@ -35,4 +43,4 @@ Developped by: El Houssaine Myaz
 > 
 > #### New Features:
 > 
-> - Build a gallery that shows the pictures taken with the phone and show it in a grid view (Library used: https://pub.dev/packages/photo_gallery) 
+> - Build a gallery that shows the pictures taken with the phone and show it in a grid view (Library used: https://pub.dev/packages/photo_gallery).
--- lib/picture_gallery.dart
+++ lib/picture_gallery.dart
@@ -7,13 +7,11 @@ import 'package:photo_gallery/photo_gallery.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class PictureGallery extends StatefulWidget {
-
   @override
   GalleryState createState() => GalleryState();
 }
 
 class GalleryState extends State<PictureGallery> {
-
   /// Initiation of variables.
   /// listOfAlbums: used to store all the albums that exist in the phone gallery in a list.
   /// cameraAlbum: used to retrieve the Camera album among the albums stored in listOfAlbums.
@@ -37,15 +35,9 @@ class GalleryState extends State<PictureGallery> {
   /// Permissions for iOS are set in Info.plist
   Future<bool> promptPermissionSetting() async {
     if (Platform.isIOS &&
-        await Permission.storage
-            .request()
-            .isGranted &&
-        await Permission.photos
-            .request()
-            .isGranted ||
-        Platform.isAndroid && await Permission.storage
-            .request()
-            .isGranted) {
+            await Permission.storage.request().isGranted &&
+            await Permission.photos.request().isGranted ||
+        Platform.isAndroid && await Permission.storage.request().isGranted) {
       return true;
     }
     return false;
@@ -55,7 +47,7 @@ class GalleryState extends State<PictureGallery> {
   Future<void> getAlbums() async {
     if (await promptPermissionSetting()) {
       List<Album> albums =
-      await PhotoGallery.listAlbums(mediumType: MediumType.image);
+          await PhotoGallery.listAlbums(mediumType: MediumType.image);
       setState(() {
         listOfAlbums = albums;
       });
@@ -83,7 +75,7 @@ class GalleryState extends State<PictureGallery> {
 
   //The method to fill the staggeredTiles list:
   void configureStaggeredView() async {
-    for (int i=0; i<pictures.length;i++){
+    for (int i = 0; i < pictures.length; i++) {
       if (i % 2 == 0) {
         staggeredTiles.add(StaggeredTile.count(2, 2));
       } else {
@@ -108,22 +100,22 @@ class GalleryState extends State<PictureGallery> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       // Group all content in a scaffold with a light blue background color:
       home: Scaffold(
         backgroundColor: Colors.blue[100],
         body: SafeArea(
-          child:Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-
-            // Page header to show page title "Picture Gallery" in a Container
-            Container(
+              // Page header to show page title "Picture Gallery" in a Container
+              Container(
                 height: 80,
 
                 // Decorate the container box by defining circular borders and gradient blue colors:
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(30), bottomLeft: Radius.circular(30)),
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(30),
+                      bottomLeft: Radius.circular(30)),
                   gradient: LinearGradient(
                     begin: Alignment.topRight,
                     end: Alignment.bottomLeft,
@@ -138,74 +130,166 @@ class GalleryState extends State<PictureGallery> {
                 ),
 
                 // Show a text "Picture Gallery" at the center of the container:
-                child:Center(
-                  child:Text(
-                  'Picture Gallery',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
+                child: Center(
+                  child: Text(
+                    'Picture Gallery',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
-                ),
                 ),
               ),
 
-            Expanded(
-              child:Container(
-              padding: EdgeInsets.all(10),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10),
 
-              // Show images in a Stagerred Grid View:
-              child: StaggeredGridView.count(
-                crossAxisCount: 4,
-                mainAxisSpacing: 4.0,
-                crossAxisSpacing: 4.0,
-                staggeredTiles: staggeredTiles,
-                children: <Widget>[
-                  ...?pictures?.map(
+                  // Show images in a Stagerred Grid View:
+                  child: StaggeredGridView.count(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 4.0,
+                    crossAxisSpacing: 4.0,
+                    staggeredTiles: staggeredTiles,
+                    children: <Widget>[
+                      ...?pictures?.map(
                         (medium) => GestureDetector(
-                      child: Container(
+                          // Call the Modal Bottom Sheet to show the picture in full view:
+                          onTap: () => showImageView(medium),
+                          child: Container(
+                            // Show a decorated box with circular borders and gradient blue colors while loading pictures:
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(18.0),
+                              gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                stops: [0.1, 0.4, 0.7, 0.9],
+                                colors: [
+                                  Colors.blue[300],
+                                  Colors.blue[400],
+                                  Colors.blue[500],
+                                  Colors.blue[600],
+                                ],
+                              ),
+                            ),
 
-                        // Show a decorated box with circular borders and gradient blue colors while loading pictures:
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(18.0),
-                          gradient: LinearGradient(
-                            begin: Alignment.topRight,
-                            end: Alignment.bottomLeft,
-                            stops: [0.1, 0.4, 0.7, 0.9],
-                            colors: [
-                              Colors.blue[300],
-                              Colors.blue[400],
-                              Colors.blue[500],
-                              Colors.blue[600],
-                            ],
+                            // Show pictures after a fading effect while keeping the circular borders:
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(18.0),
+                              child: FadeInImage(
+                                fit: BoxFit.cover,
+                                placeholder: MemoryImage(kTransparentImage),
+                                image: ThumbnailProvider(
+                                  mediumId: medium.id,
+                                  mediumType: medium.mediumType,
+                                  highQuality: true,
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-
-                        // Show pictures after a fading effect while keeping the circular borders:
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(18.0),
-                            child: FadeInImage (
-                            fit: BoxFit.cover,
-                            placeholder: MemoryImage(kTransparentImage),
-                            image: ThumbnailProvider(
-                              mediumId: medium.id,
-                              mediumType: medium.mediumType,
-                              highQuality: true,
-                            ),
-                            ),
-                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
-              ),
-            ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  // Method to show Image view in a Modal Bottom Sheet:
+  Future<void> showImageView(Medium medium) async {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        builder: (BuildContext bc) {
+          // Used an OrientationBuilder in order to control how the image view is shown in both Portrait and Landscape modes:
+          return new OrientationBuilder(builder: (context, orientation) {
+            return Padding(
+                padding: const EdgeInsets.only(
+                    top: 30.0, right: 10.0, left: 10.0, bottom: 10.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Show a "close" button at the top of the image view to close it:
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10),
+                              shape: StadiumBorder(),
+                              textStyle: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold)),
+                          child: Icon(
+                            Icons.close,
+                            color: Colors.black,
+                            size: 25.0,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                      // Show the picture in Image view with a fade in effect (Portrait orientation view):
+                      orientation == Orientation.portrait
+                          ? Container(
+                              alignment: Alignment.center,
+                              child: medium.mediumType == MediumType.image
+                                  ? ClipRRect(
+                                      borderRadius: BorderRadius.circular(18.0),
+                                      child: FadeInImage(
+                                        fit: BoxFit.fill,
+                                        placeholder:
+                                            MemoryImage(kTransparentImage),
+                                        image:
+                                            PhotoProvider(mediumId: medium.id),
+                                      ))
+                                  : Text(
+                                      'Unsupported format',
+                                      style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                            )
+                          :
+
+                          // Show the picture in Image view with a fade in effect (Landscape orientation view to avoid bottom pixel overflow):
+                          Container(
+                              alignment: Alignment.center,
+                              height: MediaQuery.of(context).size.height * 0.75,
+                              child: medium.mediumType == MediumType.image
+                                  ? ClipRRect(
+                                      borderRadius: BorderRadius.circular(18.0),
+                                      child: FadeInImage(
+                                        fit: BoxFit.fill,
+                                        placeholder:
+                                            MemoryImage(kTransparentImage),
+                                        image:
+                                            PhotoProvider(mediumId: medium.id),
+                                      ))
+                                  : Text(
+                                      'Unsupported format',
+                                      style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                            ),
+                    ]));
+          });
+        });
   }
 }
